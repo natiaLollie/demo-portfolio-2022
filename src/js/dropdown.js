bd30be60779b816f63363c1ef6eace46315d60fb
natiaLollie <natialollie.developer@gmail.com>
2022-08-20 20:23:00 -0500

    remove scratch js file

--- src/js/dropdown.js
+++
@@ -1,8 +0,0 @@
-/** script: filter by project section main tabs **/ 
-
-// function filterByDropDown() {
-
-//     var dropDownOption = dropDownSelected.options[dropDownSelected.selectedIndex];
-//     console.log(dropDownOption);
-
-// }
